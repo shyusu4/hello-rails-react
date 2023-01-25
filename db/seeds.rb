12b75805fbcf95266ac7a721b72986b92e2bede0
shyusu4 <shyusupova4@gmail.com>
2023-01-26 00:09:23 +0500

    Create greetings model and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = Greeting.create([{text: "Hello World!"}, {text: "Hello Rails!"}, {text: "Hello React!"}, {text: "Hey there!"}, {text: "Hey World!"}]) 
